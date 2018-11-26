de2cc630211b565bdd453ea8ba8bce2228b87aff
Emile NIKEL <enikel@e4r15p11.wethinkcode.co.za>
2018-11-26 16:08:18 +0200

    layout

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,12 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.all('/', function(req, res, next) {
   res.render('index', { title: 'Matcha'});
+});
+
+router.all("/feed", function(req, res) {
+  res.render('index', { title: 'Feed'});
 });
 
 module.exports = router;
